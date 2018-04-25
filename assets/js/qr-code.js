0ab5d401a7045a81f15859d408e76223bdfdb0ba
peterhoejlundandersen <hilsenit@gmail.com>
2018-04-25 17:39:11 +0200

    colorful qr-code growing

--- assets/js/qr-code.js
+++ assets/js/qr-code.js
@@ -1,11 +1,11 @@
 
 function changeImg() {
   var file_path = "/assets/blogs/digtportal/";    
-  $(".color-box").on("click mouseover", function() {
+  $(".color-box").on("click", function() {
     var new_image_file_name = file_path + $(this).attr("id") + '.jpg';
     $(".sweig-baggrund").attr("src", new_image_file_name);
   });
-  $(".back-to-org-image").on("click mouseover", function() {
+  $(".back-to-org-image").on("click", function() {
     var new_image_file_name = file_path + "baggrund-sweigs.jpg"; 
     $(".sweig-baggrund").attr("src", new_image_file_name);
   });
